421f28438b0b9114b5dd51cad6ad7d0f4606cd6b
codespace555 <namansanjaykumar@gmail.com>
2023-08-13 02:05:58 +0530

    add footer

--- newsApp/src/App.jsx
+++ newsApp/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./Components/NavBar";
 import News from "./Components/News";
+import Footersec from "./Components/Footersec";
 export default class App extends Component {
   render() {
     return (
@@ -9,6 +10,7 @@ export default class App extends Component {
         <div className="h-auto bg-light-white w-screen ">
           <NavBar />
           <News />
+          <Footersec />
         </div>
       </>
     );
---
+++ newsApp/src/Components/Footersec.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Footersec extends Component {
+  render() {
+    return (
+      <>
+ <div className="bg-green-900 mt-5 w-screen h-55 border-2">
+
+</div>
+      </>
+    );
+  }
+}
